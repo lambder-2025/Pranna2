3ee8ecb1ac12c4a7a393a1173f6d95f75ec7bb08
lambder-2025 <lalogabry3@gmail.com>
2025-09-17 14:12:01 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'loyalty-app-cache-v1';
+const ASSETS_TO_CACHE = [ './', './index.html' ];
+
+self.addEventListener('install', event => {
+  event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)) );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil( caches.keys().then(keyList => Promise.all(keyList.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }))) );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith( fetch(event.request).catch(() => caches.match(event.request)) );
+});
